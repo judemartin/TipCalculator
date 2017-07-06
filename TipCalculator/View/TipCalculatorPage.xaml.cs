7826b93f76916535fac86ea3dde6385dc0b1c667
Jude Martin <judemartin.mj@gmail.com>
2017-07-06 04:54:58 -0700

    Tip Calc

---
+++ TipCalculator/View/TipCalculatorPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace TipCalculator.View
+{
+	public partial class TipCalculatorPage : ContentPage
+    {
+        public TipCalculatorPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
